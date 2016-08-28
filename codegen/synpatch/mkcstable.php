75835ea457c57dd1500ab750499feae80384c1d3
斟酌 鵬兄 <tgckpg@gmail.com>
2016-08-28 14:52:34 +0800

    Wrong reading location

--- codegen/synpatch/mkcstable.php
+++ codegen/synpatch/mkcstable.php
@@ -4,5 +4,5 @@ $SRC_ROOT = __DIR__ . "/../native/codegen";
 include "$SRC_ROOT/mkcstable.php";
 
 $wconv_t = "#include \"pch.h\"\n";
-$wconv_t .= get_function_group("table.txt", "syn_");
-file_put_contents(__DIR__ . "/../../SynPatchConv.cpp", $wconv_t);
+$wconv_t .= get_function_group( __DIR__ . "/table.txt", "syn_" );
+file_put_contents( __DIR__ . "/../../SynPatchConv.cpp", $wconv_t );
--- codegen/vtrans/mkcstable.php
+++ codegen/vtrans/mkcstable.php
@@ -4,5 +4,5 @@ $SRC_ROOT = __DIR__ . "/../native/codegen";
 include "$SRC_ROOT/mkcstable.php";
 
 $wconv_t = "#include \"pch.h\"\n";
-$wconv_t .= get_function_group("table.txt", "w_");
-file_put_contents(__DIR__ . "/../../SymConv.cpp", $wconv_t);
+$wconv_t .= get_function_group( __DIR__ . "/table.txt", "w_" );
+file_put_contents( __DIR__ . "/../../SymConv.cpp", $wconv_t );
